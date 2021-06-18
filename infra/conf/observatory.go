bbca8ec593fbe4e23cda3a38541c0a1c717c188d
Shelikhoo <xiaokangwang@outlook.com>
2021-06-19 00:29:50 +0100

    add v4 json support for BurstObservatory & fix balancer reference

--- infra/conf/observatory.go
+++ infra/conf/observatory.go
@@ -2,6 +2,7 @@ package conf
 
 import (
 	"github.com/golang/protobuf/proto"
+	"github.com/v2fly/v2ray-core/v4/app/observatory/burst"
 
 	"github.com/v2fly/v2ray-core/v4/app/observatory"
 )
@@ -12,4 +13,18 @@ type ObservatoryConfig struct {
 
 func (o *ObservatoryConfig) Build() (proto.Message, error) {
 	return &observatory.Config{SubjectSelector: o.SubjectSelector}, nil
+}
+
+type BurstObservatoryConfig struct {
+	SubjectSelector []string `json:"subjectSelector"`
+	// health check settings
+	HealthCheck *healthCheckSettings `json:"pingConfig,omitempty"`
+}
+
+func (b BurstObservatoryConfig) Build() (proto.Message, error) {
+	if result, err := b.HealthCheck.Build(); err != nil {
+		return &burst.Config{SubjectSelector: b.SubjectSelector, PingConfig: result.(*burst.HealthPingConfig)}, nil
+	} else {
+		return nil, err
+	}
 }
